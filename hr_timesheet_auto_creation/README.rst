a940bf3cb62fcbe6fdf490132dbfbd15ad99108c
Vincent Van Rossem <vvrossem@gmail.com>
2019-12-02 13:49:35 +0100

    [ADD] hr_timesheet_auto_creation / backported from 10.0...
    ...from github : Elico-Corp 10.0/hr_timesheet_auto_creation

--- hr_timesheet_auto_creation/README.rst
+++ hr_timesheet_auto_creation/README.rst
@@ -26,7 +26,7 @@ Your location timezone).
 #. Set the user access in read the employee and read/write on
 time sheet(default the user is Administrator, So you can skip this set).
 #. Set the time or frequency as you want in the cron job:
-    1). Settings>Users>Administrator>Usability>Technical Features: check;
+    1). Click button at top right-hand corner `<User Name> -> About Odoo` and click `Activate the developer mode`
 
     2). Settings>Technical>Automation>Scheduled Actions>My current TMS:
         modify the time(`Next Execution Date`) or frequency(`Interval Number` and `Interval Unit`).
@@ -46,6 +46,7 @@ Contributors
 
 * Kevin Dong <kevin.dong@elico-corp.com>
 * Eric Caudal <eric.caudal@elico-corp.com>
+* Vincent Van Rossem <vincent@coopiteasy.be>
 
 Maintainer
 ----------
--- hr_timesheet_auto_creation/models/hr_timesheet_sheet.py
+++ hr_timesheet_auto_creation/models/hr_timesheet_sheet.py
@@ -1,33 +1,36 @@
 # -*- coding: utf-8 -*-
 # © 2016-2017 Elico Corp (https://www.elico-corp.com)
+# Copyright 2019 Coop IT Easy SCRLfs
+#   - Vincent Van Rossem <vincent@coopiteasy.be>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import api, models
+from openerp import api, models
 
 from datetime import datetime, timedelta
 
 
 class HrTimesheetSheet(models.Model):
-    _inherit = 'hr_timesheet_sheet.sheet'
+    _inherit = "hr_timesheet_sheet.sheet"
 
     @api.model
     def create_employee_timesheet(self):
-        employee_obj = self.env['hr.employee']
-        employee_ids = employee_obj.search([('active', '=', True),
-                                            ('user_id', '!=', False),
-                                            ]).ids
+        employee_obj = self.env["hr.employee"]
+        employee_ids = employee_obj.search(
+            [("active", "=", True), ("user_id", "!=", False)]
+        ).ids
         today = datetime.now()
         monday = today + timedelta(days=-today.weekday())
         sunday = monday + timedelta(days=+6)
         # Search for existing timesheet
-        exists_timesheet_records = self.search([('date_to', '>=', monday)])
-        ignore_employee_ids = map(lambda x: x.employee_id.id,
-                                  exists_timesheet_records)
+        exists_timesheet_records = self.search([("date_to", ">=", monday)])
+        ignore_employee_ids = map(
+            lambda x: x.employee_id.id, exists_timesheet_records
+        )
         employee_ids = list(set(employee_ids) - set(ignore_employee_ids))
         vals = {
-            'date_from': monday.strftime('%Y-%m-%d'),
-            'date_to': sunday.strftime('%Y-%m-%d')
+            "date_from": monday.strftime("%Y-%m-%d"),
+            "date_to": sunday.strftime("%Y-%m-%d"),
         }
         for employee_id in employee_ids:
-            vals['employee_id'] = employee_id
+            vals["employee_id"] = employee_id
             self.sudo().create(vals)
         return True
