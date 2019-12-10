23e071610a3484363a2647b011f92e4961265f5b
Vincent Van Rossem <vvrossem@gmail.com>
2019-12-10 14:54:20 +0100

    [REM] hr_timesheet_overtime : unused test

--- hr_timesheet_overtime/tests/test_overtime.py
+++ hr_timesheet_overtime/tests/test_overtime.py
@@ -19,8 +19,3 @@ class TestOvertime(TransactionCase):
         self.pp_contract1 = self.env.ref("pieter_parker_contract1")
         self.pp_contract2 = self.env.ref("pieter_parker_contract2")
 
-    def test_initial_overtime(self):
-        initial_overtime = self.timesheet_sheet_model._get_initial_overtime()
-        self.assertEquals(
-            initial_overtime, 0.0, "Initial Overtime has a 0.0 default value"
-        )
