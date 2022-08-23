2bd9fe0d865968adad25b580ba2f19e4c0d57426
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-08-23 11:45:38 +0200

    [ADD] hr_timesheet_project_prevent_creation
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

---
+++ hr_timesheet_project_prevent_creation/__manifest__.py
@@ -0,0 +1,27 @@
+# SPDX-FileCopyrightText: 2022 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+    "name": "Prevent creation of projects and tasks from timesheets",
+    "summary": """
+        Prevent creation of projects and tasks from timesheets.""",
+    "version": "12.0.1.0.0",
+    "category": "Human Resources",
+    "website": "https://coopiteasy.be",
+    "author": "Coop IT Easy SC",
+    "maintainers": ["carmenbianca"],
+    "license": "AGPL-3",
+    "application": False,
+    "depends": [
+        "hr_timesheet",
+        "hr_timesheet_sheet",
+    ],
+    "excludes": [],
+    "data": [
+        "views/hr_timesheet_sheet_views.xml",
+        "views/hr_timesheet_views.xml",
+    ],
+    "demo": [],
+    "qweb": [],
+}
